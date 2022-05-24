61695773f905a7e4a298957ae01d89c4dc7be2e0
kuhataku <kuhataku@gmail.com>
2022-05-24 22:55:06 +0900

    一応動いた

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,2 @@
-INSERT INTO `user` (id, account, email, admin, password) VALUES (1, 'admin', 'admin@example.com', TRUE, '{bcrypt}$2a$10$u1heCOmGzy3zuRJb253sb.9I.4bbE0ZeGS2xUbayV1eXgbZDr.4ui');
-INSERT INTO `user` (id, account, email, admin, password) VALUES (2, 'test', 'test@example.com', FALSE, '{bcrypt}$2a$10$yhKS51Zk5PPSFh4aFnCps.tlxHXonHjs8DBIpa5PG9Setq33z2qcy');
+INSERT INTO "user" (id, account, email, admin, password) VALUES (1, 'admin', 'admin@example.com', TRUE, '{bcrypt}$2a$10$u1heCOmGzy3zuRJb253sb.9I.4bbE0ZeGS2xUbayV1eXgbZDr.4ui');
+INSERT INTO "user" (id, account, email, admin, password) VALUES (2, 'test', 'test@example.com', FALSE, '{bcrypt}$2a$10$yhKS51Zk5PPSFh4aFnCps.tlxHXonHjs8DBIpa5PG9Setq33z2qcy');
